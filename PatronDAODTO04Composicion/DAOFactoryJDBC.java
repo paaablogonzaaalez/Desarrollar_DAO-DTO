5e4840dd2ed253d52a2fe7dbf4c7df6ef89d0c95
paaablogonzaaalez <pgonzalezn13@iesarroyoharnina.es>
2026-01-14 18:42:06 +0100

    Implement DAOFactoryJDBC for JDBC data access

---
+++ PatronDAODTO04Composicion/DAOFactoryJDBC.java
@@ -0,0 +1,29 @@
+package modelo.ImplDAO;
+
+import java.sql.SQLException;
+
+import modelo.AbstractDAO.DaoFactory;
+import modelo.AbstractDAO.PersonaDAO;
+import modelo.acceso.AccessJdbc;
+
+public class DAOFactoryJDBC extends DaoFactory {
+	private AccessJdbc accessJdbc;
+	private GenericComponentDAOJDBC genericComponentDAOJDBC;
+	
+	public DAOFactoryJDBC() throws ClassNotFoundException, SQLException {
+		super();
+		// EStos parametros pueden ser pasado por parametros o configurados en otro sitio
+		accessJdbc = new AccessJdbc("ejemplo", "harnina", "zzzz");
+		genericComponentDAOJDBC = new GenericComponentDAOJDBC(accessJdbc);
+	}
+
+	@Override
+	public PersonaDAO getPersonaDAO() {
+		return new PersonaDAOJdbc(genericComponentDAOJDBC);
+	}
+
+	@Override
+	public AlumnoDAOJDBC getAlumnoDAO() {
+		return new AlumnoDAOJDBC(genericComponentDAOJDBC);
+	}
+}
